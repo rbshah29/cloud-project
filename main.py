61db428e445469da07ca819f66a9ec31f02c6817
Rutvik Shah <55599346+rbshah29@users.noreply.github.com>
2023-07-13 21:43:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ S3_BUCKET = 'check-file-lambda'
 aws_access_key_id='ASIA3GA4UEJCEUTK4R33'
 aws_secret_access_key='w4eb2D19dPHfY0dOExcAG57MJLbIW0dGRjmhd91w'
 aws_session_token='FwoGZXIvYXdzEI///////////wEaDNysAuuCOvU9Ge5+nCLAAfY2ZBDRXTb8Y4bfJ35/yngo5LuoLzaNj6PgiPthLgVKPArSRlHmBPd5eQfeu6VZej0uccdp56pCpjo6FtStekoyBQIV3R7CNE9U+5sjVPNW7pd2PduquK7erOY+GXy/24Ruq6ISbQA9x/053VMhoGUEerPwPyWAvZqAEQSTod5xA9M7r49okXpg0HuE6U7jlKxo4fqJpFQEkJOUOn2OByru6MW5w5v47N87Kx+6nP0nUTY1jMpnj37l9ntc8Q0mRyjX1MGlBjItzrDmSQtE4M8Ayp5y9VEs6SjcM2IvrC4rvMEsJHVhTqBsHDrsdUxa4q9l2qqd'
+region_name="us-east-1"
 
 
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token,region_name=region_name)
 
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token)
-
-dynamodb = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token)
+dynamodb = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token,region_name=region_name)
 
 @app.route('/')
 def index():
